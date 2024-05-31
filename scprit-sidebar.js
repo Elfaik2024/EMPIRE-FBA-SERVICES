436375fbf897d7664acab2f52c8bbf5e225bea96
Elfaik2024 <ochiha.sasuke.2011@gmail.com>
2024-06-01 00:05:02 +0100

    add slidebar and button scroll

---
+++ scprit-sidebar.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+var element = document.getElementById('sidebar');
+ 
+function showSlidebar(){
+
+    element.classList.toggle('show');
+    
+}
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
